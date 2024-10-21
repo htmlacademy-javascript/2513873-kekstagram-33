5e8cbe8ab664a548627ec0504aff3fba507958d1
Вероника Неброева <v-kleo@yandex.ru>
2024-10-21 19:12:23 +0700

    Исправляет по линтерам

--- js/functions.js
+++ js/functions.js
@@ -1,55 +1,58 @@
 // Функция проверки длины строки
 
-const stringName = 'Проверяющая строка для теста функции';
+const stringText = 'Проверяющая строка для теста функции';
 
-function getLengthString (stringName, stringLength) {
+function getLengthString(stringName, stringLength) {
   if (stringLength > 10 & stringLength <= 50) {
     return true;
   }
-    return false;
+  return false;
 }
 
-console.log(getLengthString(stringName, stringName.length))
+getLengthString(stringText, stringText.length);
 
 // Функция проверки на палиндромность
 let stringTest = '';
 let result = '';
 
-function getStringSame (string) {
+function getStringSame(string) {
   const stringNoSpaces = string.replaceAll(' ', '');
   const stringNormalize = stringNoSpaces.toLowerCase();
 
 
   // Не смогла добиться результата этим способом
-  //   for (let i = stringNormalize.length - 1; i > stringNormalize[0]; i--) {
-  //     stringTest += stringNormalize[i];
-  //   }
+  for (let i = stringNormalize.length - 1; i > stringNormalize[0]; i--) {
+    stringTest += stringNormalize[i];
+  }
 
-  stringTest = stringNormalize.split("").reverse().join("");
+  // А на этот выдаёт ошибку "Strings must use singlequote", хотя всё работает
+  // stringTest = stringNormalize.split("").reverse().join("");
 
   if (stringNormalize === stringTest) {
     result = true;
-  } else {result = false}
+  } else {
+    result = false;
+  }
 
   return result;
 }
 
-console.log(getStringSame('Лёша на полке клопа нашёл '));
+getStringSame('Лёша на полке клопа нашёл ');
 
 // Попытка создания функция извлечения чисел из строк
 let stringResult = '';
 let stringParse = '';
 
-function getNumbers (stringToNumber) {
-    stringToNumber.toString();
+function getNumbers(stringToNumber) {
+  stringToNumber.toString();
   for (let i = 0; i <= stringToNumber.length - 1; i++) {
-      if (stringToNumber[i].isNaN) {
-          continue
-      } else {
-          stringParse += stringToNumber[i];
-          stringResult = parseInt(stringParse, 10);
-      }
+    if (stringToNumber[i].isNaN) {
+      continue;
+    } else {
+      stringParse += stringToNumber[i];
+      stringResult = parseInt(stringParse, 10);
+    }
   }
   return stringResult;
 }
-console.log(getNumber('2024 год очень знаменателен, в отличие от 2023го'));
+getNumbers('2024 год очень знаменателен, в отличие от 2023го');
