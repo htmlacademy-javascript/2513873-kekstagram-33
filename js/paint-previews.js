1f0330e0b24a7a17ded8c8059c90b6d198afaec1
Вероника Неброева <v-kleo@yandex.ru>
2024-11-12 20:43:00 +0700

    Эксперимент с извлечением данных объекта из массива comments

--- js/paint-previews.js
+++ js/paint-previews.js
@@ -10,7 +10,25 @@ crateUsersPhotos.forEach(({url, description, comments, likes}) => {
   const userPhotoContainer = templatePicture.cloneNode(true);
   userPhotoContainer.querySelector('.picture__img').src = url;
   userPhotoContainer.querySelector('.picture__img').alt = description;
-  userPhotoContainer.querySelector('.picture__comments').textContent = comments;
+
+  for (let i = 0; i < comments.length; i++) {
+    const pictureCommentsBlock = userPhotoContainer.querySelector('.picture__comments');
+
+    const pictureCommentData = document.createElement('p');
+    pictureCommentData.id = comments[i].id;
+    const commentAvatar = document.createElement('img');
+    commentAvatar.src = comments[i].avatar;
+    pictureCommentData.appendChild(commentAvatar);
+    const commentText = document.createElement('span');
+    commentText.textContent = comments[i].message;
+    pictureCommentData.appendChild(commentText);
+    const commentUserName = document.createElement('span');
+    commentUserName.textContent = comments[i].name;
+    pictureCommentData.appendChild(commentUserName);
+
+    pictureCommentsBlock.appendChild(pictureCommentData);
+  };
+
   userPhotoContainer.querySelector('.picture__likes').textContent = likes;
 
   fragment.appendChild(userPhotoContainer);
