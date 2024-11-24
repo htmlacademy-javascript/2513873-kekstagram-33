26bdb47ecc189a9e0887c8a34a059974e95ac851
Вероника Неброева <v-kleo@yandex.ru>
2024-11-24 15:08:22 +0700

    Действительно блокирует отправку формы при ошибке

--- js/form.js
+++ js/form.js
@@ -7,7 +7,6 @@ const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
 const uploadCancelButton = uploadForm.querySelector('.img-upload__cancel');
 const uploadHashtag = uploadForm.querySelector('.text__hashtags');
 const uploadComment = uploadForm.querySelector('.text__description');
-const uploadButton = uploadForm.querySelector('.img-upload__submit');
 
 const HASHTAGS_MAXCOUNT = 5;
 const COMMENT_MAXLENGTH = 140;
@@ -16,7 +15,7 @@ const HASHTAG_MAXLENGTH = 20;
 const errorMessages = {
   INVALID_HASHTAG_STRING: 'Хэш-тег должен начинаться с #, состоять из букв и чисел без пробелов',
   COMMENT_MAXLENGTH_ERROR: `Максимальная длина комментария ${COMMENT_MAXLENGTH} символов`,
-  COUNT_ERROR: `Нельзя указать больше ${HASHTAG_MAXLENGTH} хэш-тегов`,
+  COUNT_ERROR: `Нельзя указать больше ${HASHTAGS_MAXCOUNT} хэш-тегов`,
   HASHTAG_MAXLENGTH_ERROR: `Максимальная длина хэш-тега ${HASHTAG_MAXLENGTH} символов`,
   UNIQUENESS_ERROR: 'Хэш-теги не должны повторяться',
 };
@@ -25,26 +24,20 @@ const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'img-upload__error-text'
-});
+}, false);
 
-// Проверка тэгов
+// Проверка хэштэгов
 
 const getHashtags = (value) => {
   const hashtags = value.trim().split(/\s+/);
   return hashtags;
 };
 
-const checkSymbols = (value) => {
-  getHashtags(value).every((hashtag) => VALID_HASHTAG_STRING.test(hashtag));
-};
+const checkSymbols = (value) => getHashtags(value).every((hashtag) => VALID_HASHTAG_STRING.test(hashtag));
 
-const checkLength = (value) => {
-  getHashtags(value).every((hashtag) => hashtag.length <= HASHTAG_MAXLENGTH);
-};
+const checkLength = (value) => getHashtags(value).every((hashtag) => hashtag.length <= HASHTAG_MAXLENGTH);
 
-const checkCount = (value) =>
-  getHashtags(value).length <= HASHTAGS_MAXCOUNT;
-
+const checkCount = (value) => getHashtags(value).length <= HASHTAGS_MAXCOUNT;
 
 const checkUniqueness = (value) => {
   const hashtags = getHashtags(value);
@@ -53,7 +46,7 @@ const checkUniqueness = (value) => {
 };
 
 pristine.addValidator(uploadHashtag, checkSymbols, errorMessages.INVALID_HASHTAG_STRING);
-pristine.addValidator(uploadHashtag, checkLength, errorMessages.LENGTH_ERROR);
+pristine.addValidator(uploadHashtag, checkLength, errorMessages.HASHTAG_MAXLENGTH_ERROR);
 pristine.addValidator(uploadHashtag, checkCount, errorMessages.COUNT_ERROR);
 pristine.addValidator(uploadHashtag, checkUniqueness, errorMessages.UNIQUENESS_ERROR);
 
@@ -78,31 +71,19 @@ const onEditingFormEscKeydown = (evt) => {
 
 // Открытие формы редактирования
 const openEditingForm = () => {
-  uploadInput.addEventListener('change', () => {
+  uploadInput.addEventListener('change', (evt) => {
     uploadOverlay.classList.remove('hidden');
     document.addEventListener('keydown', onEditingFormEscKeydown);
     body.classList.add('modal-open');
     uploadCancelButton.addEventListener('click', closeEditingForm);
   });
-  /*if (!checkSymbols || !checkLength || !checkCount || !checkUniqueness || !checkComment) {
-    uploadButton.classList.add('disabled');
-  };
-  if (!pristine) {
-    uploadButton.classList.add('disabled');
-  }*/
 };
 
-const submitForm = () => {
-  uploadForm.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    if (pristine.validate()) {
-      uploadForm.submit();
-    }
-  });
-};
-
-uploadButton.addEventListener('click', () => {
-  submitForm();
+uploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    uploadForm.submit();
+  }
 });
 
 // Закрытие формы
